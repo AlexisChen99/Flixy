0beaf5040bc8c346eca717f7b26b917ff831da05
Alexis Chen <anc009@its-gl1w-mac10.ucsd.edu>
2020-01-29 19:55:37 -0800

    first commit

---
+++ Flixy/MovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Movie MovieViewController.swift
+//  Flixy
+//
+//  Created by Alexis Chen on 1/29/20.
+//  Copyright © 2020 Alexis Chen. All rights reserved.
+//
+
+import UIKit
+
+class Movie_MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
